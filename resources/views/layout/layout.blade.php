205633743451fa4afdee9f03d46881c37080119b
Siddharth Sharma <siddharthsharmaofficial2@gmail.com>
2025-09-13 14:33:46 +0530

    removed preloader

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -1,9 +1,9 @@
 @include("layout.header")
 <body
     class="page-template page-template-page-templates page-template-home-dark-template page-template-page-templateshome-dark-template-php page page-id-262  dark-bg elementor-default elementor-kit-6 elementor-page elementor-page-262">
-    <div id="loading">
+    {{-- <div id="loading">
         <div class="load-circle"><span class="one"></span></div>
-    </div>
+    </div> --}}
 
     <main class="wrapper">
         <div data-elementor-type="wp-page" data-elementor-id="262" class="elementor elementor-262">
