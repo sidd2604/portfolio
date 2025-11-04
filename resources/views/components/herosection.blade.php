8167afbf178255e7eaf391c44cef68b6e2f20de1
Siddharth Sharma <siddharthsharmaofficial2@gmail.com>
2025-11-05 01:40:19 +0530

    minor change

--- resources/views/components/herosection.blade.php
+++ resources/views/components/herosection.blade.php
@@ -15,7 +15,7 @@
                 <div class="row min-vh-100 align-items-center gy-5">
                   <div class="col-lg-7 pe-lg-5">
                     <div class="home-intro">
-                      <h6>My Introduction</h6>
+                      <h6>Introduction</h6>
                       <h2>
                         I'm Rajat , a <span class="text-theme">Laravel Developer</span>
                       </h2>
